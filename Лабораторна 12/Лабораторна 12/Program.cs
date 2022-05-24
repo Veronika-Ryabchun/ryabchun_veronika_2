ddb3a950b75197850cd9762a6b7a75efad2badaf
Veronika-Ryabchun <nikakyiv696@gmail.com>
2022-05-24 21:22:43 +0300

    Додала 2 роботу

---
+++ Лабораторна 12/Лабораторна 12/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Laboratorna_12
+{
+    class Myclass
+    {
+        public string First;
+        public string Second;
+        public Myclass()
+        {
+            First = "first";
+            Second = "second";
+        }
+
+        public Myclass(string t, string a)
+        {
+            First = t;
+            Second = a;
+        }
+        ~Myclass()
+        {
+            Console.WriteLine("Destructor was called");
+        }
+        public string Ser( )
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+        static public Myclass Des(string json)
+        {
+            return JsonConvert.DeserializeObject<Myclass>(json);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Myclass D = new Myclass();
+            Console.WriteLine(D.First);
+            Myclass B = new Myclass("A", "B");
+            Console.WriteLine(B.Second);
+            string js = D.Ser();
+            Console.WriteLine("json: " + js);
+            string json = @"{ 'First':'Tom','Second':'Jackson' }";
+            Myclass F = Myclass.Des(json);
+            Console.WriteLine(F.Second);
+        }
+    }
+
+}
